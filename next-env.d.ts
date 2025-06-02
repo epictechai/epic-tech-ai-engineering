3a26eb258817e180c81d652e12336dc2e7d75507
Epic Tech <epictechai@gmail.com>
2025-06-01 21:18:09 -0400

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// &lt;reference types="next" /&gt;
+/// &lt;reference types="next/image-types/global" /&gt;
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
