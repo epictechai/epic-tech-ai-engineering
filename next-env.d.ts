af70d7b33612e0bd2cfbc2d3de09c989724fc7a8
epic <epictechai@gmail.com>
2025-06-01 22:42:33 -0400

    1

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,5 @@
-/// 
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
 
-reference
-///
-
-
-reference
-// NOTE: This file should not be edited // see https://nextjs.org/docs/basic-features/typescript for more information.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
